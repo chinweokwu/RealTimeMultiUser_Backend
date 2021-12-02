047a7b590fb98292b0c122ad9d4f3d763dae2561
chinweokwu <paulcmorah@gmail.com>
2021-12-03 00:52:48 +0100

    update routes and connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+    def connect
+      self.current_user = find_verified_user request.params[:token]
+      logger.add_tags 'ActionCable', current_user.id
+    end
+
+    private
+    
+    def find_verified_user token
+      session = Session.find_by(token: token)
+      unless session.nil?
+        return session.user
+      else
+        return reject_unauthorized_connection
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post 'login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   get "/auto_login", to: "sessions#auto_login"
+  mount ActionCable.server => '/cable'
 end
