df75139c9bc8f606428cf63c515ae9ca3f0c4c2f
chinweokwu <paulcmorah@gmail.com>
2021-12-03 01:34:35 +0100

    add connection to web socket

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,16 +2,16 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
     def connect
-      self.current_user = find_verified_user request.params[:token]
+      self.current_user = find_verified_user
       logger.add_tags 'ActionCable', current_user.id
     end
 
     private
     
-    def find_verified_user token
-      session = Session.find_by(token: token)
-      unless session.nil?
-        return session.user
+    def find_verified_user
+      current_user = User.find_by(auth_token: request.params[:auth_token])
+      unless current_user.nil?
+        return current_user
       else
         return reject_unauthorized_connection
       end
